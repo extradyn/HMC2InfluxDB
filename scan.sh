1915a48258a3889175ccc2d0aa68ff512a8046cf
extraDYN <extradyn@extradyn.com>
2018-01-30 22:21:36 -0500

    Performance metrics gathering

--- scan.sh
+++ scan.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 . config.cfg
 listManaged=$(ssh $HMCuser@$HMCserver "lssyscfg -r sys -F name")
-for i in $listManaged; do
-	ssh $HMCuser@$HMCserver "lssyscfg -r lpar -m $i"
+for managedServer in $listManaged; do
+	ssh $HMCuser@$HMCserver "lssyscfg -r lpar -m $managedServer" >/tmp/$HMCserver.$managedServer.lssyscfg.tmp
+	ssh $HMCuser@$HMCserver "lslparutil -m $managedServer -r all -h 2" >/tmp/$HMCserver.$managedServer.lslparutil.tmp
 done
 
