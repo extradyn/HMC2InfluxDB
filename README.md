e92fc1874fb8bea02e0b6817f95344fee6c7d6b7
Extradyn <extradyn@extradyn.com>
2018-01-31 19:12:47 -0500

    Fixed markdown syntax. Fixed double quotes.

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 HMC pSeries Capacity and Performance Statistics to influxDB Import Tool
 
 Create a small configuration file "config.cfg" with following variables:
+<br/>
+<code>
 HMCuser=<ssh user on HMC>
+<br>
 HMCserver=<HMC server to connect to>
+</code>
--- scan.sh
+++ scan.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . config.cfg
-echo “Get Managed Systems List”
+echo "Get Managed Systems List"
 listManaged=$(ssh $HMCuser@$HMCserver "lssyscfg -r sys -F name")
-echo “Process Managed Systems”
+echo "Process Managed Systems"
 for managedServer in $listManaged; do
-    echo “Processing : $managedServer”
+    echo "Processing : $managedServer"
 	ssh $HMCuser@$HMCserver "lssyscfg -r lpar -m $managedServer" >/tmp/$HMCserver.$managedServer.lssyscfg.tmp
 	ssh $HMCuser@$HMCserver "lslparutil -m $managedServer -r all -h 2" >/tmp/$HMCserver.$managedServer.lslparutil.tmp
 done
-echo “Done”
+echo "Done"
