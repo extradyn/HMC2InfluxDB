2c6bbfb619c98630dfa6849bdaceca296bbeb7ed
Extradyn <extradyn@extradyn.com>
2018-02-01 23:16:57 -0500

    more fields

--- hmc2influxdb.sh
+++ hmc2influxdb.sh
@@ -12,8 +12,7 @@ for hmcServer in "${hmcArray[@]}"; do
 	echo "Process Managed Server on HMC $hmcServer"
 	for managedServer in $listManaged; do
 		echo "Processing : $managedServer"
-		ssh $HMCuser@$hmcServer "lslparutil -r lpar -m $managedServer -h 1 -F time,sys_time,lpar_name,entitled_cycles,capped_cycles,uncapped_cycles --filter \"event_types=sample\"" | awk -v managedServer="$managedServer" -F "," '{ OFS=FS; cmd="date -d\"" $1 "\" +%s"; cmd | getline $1; close(cmd) ; print "lslparutil,managedSystem="managedServer",lpar_name="$3" entitled_cycles="$4",capped_cycles="$5",uncapped_cycles="$6" "$1"000000000"}' >/tmp/$hmcServer.$managedServer.lslparutil.out
-
+		ssh $HMCuser@$hmcServer "lslparutil -r lpar -m $managedServer -h 1 -F time,sys_time,lpar_name,entitled_cycles,capped_cycles,uncapped_cycles,curr_proc_mode,curr_sharing_mode,curr_uncap_weight,curr_shared_proc_pool_name,mem_mode,curr_mem,curr_procs,curr_proc_units --filter \"event_types=sample\"" | awk -v managedServer="$managedServer" -F "," '{ OFS=FS; cmd="date -d\"" $1 "\" +%s"; cmd | getline $1; close(cmd) ; print "lslparutil,managedSystem="managedServer",lpar_name="$3",curr_proc_mode="$7",curr_sharing_mode="$8",curr_uncap_weight="$9",curr_shared_proc_pool_name="$10",mem_mode="$11" curr_mem="$12",curr_procs="$13",curr_proc_units="$14",entitled_cycles="$4",capped_cycles="$5",uncapped_cycles="$6" "$1"000000000"}' >/tmp/$hmcServer.$managedServer.lslparutil.out
 		curl --data-binary "@/tmp/$hmcServer.$managedServer.lslparutil.out" -X POST "http://localhost:8086/write?db=hmc&u=root&p=root"
 		
 	done
